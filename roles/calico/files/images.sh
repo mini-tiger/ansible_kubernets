5cc1bfbe4df0a85613673f9fe4f376278d3c8a9a
root <root@192.168.254.133>
2023-12-07 16:56:16 +0800

    tt

---
+++ roles/calico/files/images.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#set -ex && set -o pipefail
+registry_addr=$1:5000
+pushd $2
+for i in `cat image_list` 
+do 
+   image_name=$(echo $i|cut -d "/" -f 3)
+   echo $image_name
+   echo $i
+   ctr image pull $i
+   ctr image tag $i ${registry_addr}/calico/${image_name}
+   ctr i push --skip-verify --platform amd64  --plain-http ${registry_addr}/calico/${image_name}
+   # Check if the push was successful
+   if [ $? -ne 0 ]; then
+       echo "Failed to push image: ${registry_addr}/calico/${image_name}"
+       exit 1  # Exit the script with a non-zero status
+   fi
+done
+popd
+exit 0
